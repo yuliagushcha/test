8b1547341247b0b47cc5ad066875ed65555396ee
yuliagushcha <Yulia_Gushcha@mail.ru>
2021-03-11 23:24:32 +0300

    feat: implement saving to local storage and clearing of the tags

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 // adding tags
+
 let addButton = document.querySelector('.button');
 
 addButton.addEventListener('click', () => {
@@ -22,21 +23,39 @@ addButton.addEventListener('click', () => {
     document.querySelector('.inputValue').value = ''; 
   }
 
+  let count = document.querySelectorAll('.tag').length;
+  localStorage.setItem(`tag${count}`, `${inputValue}`);
+
 });
 
 
 // tags removal
+
 document.addEventListener( "click" , ({target}) => (target = target.closest(".pic")) && target.parentElement.remove());
 
 
 // readonly mode
+
 let active = document.querySelector('.readOnlyMode');
 let textMode = document.querySelector('.inputValue');
 let tagMode = document.querySelector('.component_TagsArea');
+let clearButton = document.querySelector('.clearTags');
 
 active.addEventListener('click', () => {
   (!active.classList.contains('active')) ? active.classList.add('active') : active.classList.remove('active');
   (!textMode.hasAttribute('readonly')) ? textMode.setAttribute('readonly', '') : textMode.removeAttribute('readonly');
   (!addButton.hasAttribute('disabled')) ? addButton.setAttribute('disabled', '') : addButton.removeAttribute('disabled');
-  (!tagMode.classList.contains('inactiveTag')) ? tagMode.classList.add('inactiveTag') : tagMode.classList.remove('inactiveTag')
+  (!tagMode.classList.contains('inactiveTag')) ? tagMode.classList.add('inactiveTag') : tagMode.classList.remove('inactiveTag');
+  (!clearButton.classList.contains('inactiveTag')) ? clearButton.classList.add('inactiveTag') : clearButton.classList.remove('inactiveTag');
+})
+
+
+// adding to local storage, clearing tags
+
+clearButton.addEventListener('click', () => {
+  localStorage.clear();
+  let tagsParent = document.querySelector('.component_TagsArea');
+  while (tagsParent.firstChild) {
+    tagsParent.removeChild(tagsParent.firstChild);
+  }
 })
