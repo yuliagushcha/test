e3046b39b7aabbd02b5c7bd58e316c1362e016ea
yuliagushcha <Yulia_Gushcha@mail.ru>
2021-03-11 17:43:47 +0300

    fix: fix shortcomings in adding tags

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 
 let addButton = document.querySelector('.button');
+let closeButton = document.querySelector('.pic');
 
 addButton.addEventListener('click', () => {
+
   let tagsArea = document.querySelector('.component_TagsArea');
   let inputValue = document.querySelector('.inputValue').value;
-  let tagContainer = document.createElement('p');
+  let tagContainer = document.createElement('div');
+  let pic = document.createElement('img');
+
+  pic.className = 'pic';
+  pic.src = 'cross.png';
   tagContainer.className = 'tag';
-  tagContainer.innerHTML = `${inputValue}`;
-  tagsArea.prepend(tagContainer);
-  document.querySelector('.inputValue').value = ''; 
+
+  if (inputValue === '') {
+    alert('Type the tag');
+  } else {
+    tagContainer.innerHTML = `<p>${inputValue}</p>`;
+    tagContainer.append(pic);
+    tagsArea.prepend(tagContainer);
+    document.querySelector('.inputValue').value = ''; 
+  }
+  
 });
+
+// if (closeButton) {
+//   closeButton.addEventListener('click', (i) => {
+//     closeButton.style.display = 'none';
+//   })
+// }
 
